3ddff331b7dd5c00aabaaf133640b37e51885ed4
Vishal <vishalchaudhari4530@gmail.com>
2025-03-11 16:16:07 +0530

    changes emp-list

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
+app.use(cors({ origin: 'http://localhost:4200' }));
 
 // Database Connection with better error handling
 mongoose.connect(process.env.MONGO_URI, { 
